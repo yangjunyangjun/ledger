966077696b0f3cc2a03a524a7ee44b76f5be67be
ex_yangjun2 <1752641276@qq.com>
2021-07-15 23:17:26 +0800

    user auth

--- pkg/server/middlware/auth.go
+++ pkg/server/middlware/auth.go
@@ -1,6 +1,7 @@
 package middlware
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"ledger/pkg/server/webbase"
 	"ledger/sdk"
@@ -10,10 +11,14 @@ import (
 func CheckLogin() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		auth := c.GetHeader("Authorization")
-		var u *webbase.Claims
+		var (
+			u   *webbase.Claims
+			err error
+		)
 
 		//从redis获取用户信息
-		if err := sdk.RedisCli.Get(auth).Scan(&u); err != nil {
+		userByte, err := sdk.RedisCli.Get(auth).Bytes()
+		if err != nil || userByte == nil {
 			//redis不存在解析token
 			u, err = webbase.ParseToken(auth)
 			//解析失败代表token过期
@@ -22,11 +27,30 @@ func CheckLogin() gin.HandlerFunc {
 				c.Abort()
 				return
 			}
+			//将用户信息转为byte
+			userByte, err = json.Marshal(u)
+			if err != nil {
+				sdk.Log.Errorf("Marshal  error %v ", err)
+				c.JSON(401, "API token required")
+				c.Abort()
+				return
+			}
+
+		} else {
+			if err = json.Unmarshal(userByte, &u); err != nil {
+				sdk.Log.Errorf("UmMarshal  error %v ", err)
+				c.JSON(401, "API token required")
+				c.Abort()
+				return
+
+			}
 		}
+
 		// redis有用户信息 或 token解析成功，将用户信息set到redis
-		if err := sdk.RedisCli.Set(auth, u, time.Hour*24*1).Err(); err != nil {
+		if err := sdk.RedisCli.Set(auth, userByte, time.Hour*24*1).Err(); err != nil {
 			sdk.Log.Errorf("redis set error %v ", err)
-			c.JSON(5000, "redis set err")
+			c.JSON(401, "redis set err")
+			c.Abort()
 			return
 		}
 		c.Set("user", u)
